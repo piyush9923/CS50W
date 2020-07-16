528ea94f1e09995ac6869c487c1aa40a2f6ce453
Piyush <piyushkhandelwal1104@gmail.com>
2020-07-16 20:16:31 +0530

    Updated Pages and functions
    CSS remaining

--- Project_1/encyclopedia/urls.py
+++ Project_1/encyclopedia/urls.py
@@ -5,5 +5,9 @@ from . import views
 app_name = "wikipedia"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.info, name='title')
+    path("wiki/<str:title>", views.info, name='title'),
+    path("search", views.search, name='search'),
+    path("create", views.create, name='create'),
+    path("random", views.random_page, name='random'),
+    path("wiki/<title>/edit", views.edit, name='edit')
 ]
--- Project_1/encyclopedia/views.py
+++ Project_1/encyclopedia/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+import random
+
+from django import forms
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 
 from . import util
 import markdown2
+
+
+class NewPage(forms.Form):
+    title = forms.CharField(label='Title')
+    content = forms.CharField(label='Description', widget=forms.Textarea(attrs={'cols': '60', 'style': 'height: 250px;'}))
 
 
 def index(request):
@@ -19,3 +30,59 @@ def info(request, title):
         })
     else:
         return render(request, "encyclopedia/error.html")
+
+
+def search(request):
+    if request.method == "POST":
+        query = request.POST.get('q')
+        entry_list = util.list_entries()
+        if query in entry_list:
+            return render(request, "encyclopedia/search.html", {
+                "content": markdown2.markdown(util.get_entry(query)),
+            })
+        else:
+            final_list = [a for a in entry_list if query in a]
+            return render(request, "encyclopedia/index.html", {
+                "entries": final_list
+            })
+
+def create(request):
+    if request.method == "POST":
+        form = NewPage(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            content = form.cleaned_data['content']
+            if title in util.list_entries():
+                return HttpResponse("Unable to Save! "+title+" already exists")
+            util.save_entry(title, content)
+            return render(request, "encyclopedia/create.html", {
+                "form": NewPage(),
+                'success': True
+            })
+
+    return render(request, "encyclopedia/create.html", {
+        "form": NewPage(),
+        'success': False
+    })
+
+def random_page(request):
+    title = random.choice(util.list_entries())
+    raw_html = markdown2.markdown(util.get_entry(title))
+    return render(request, "encyclopedia/main.html", {
+        "title": title,
+        "text": raw_html
+    })
+
+
+def edit(request, title):
+    entry = util.get_entry(title)
+    if entry:
+        if request.method == 'POST':
+            entry = request.POST.get('body').strip()
+            if len(entry) > 10:
+                util.save_entry(title, entry)
+                return HttpResponseRedirect(reverse_lazy('load_page', args=[title]))
+            else:
+                messages.error(request, f'New Entry Don\'t have enough characters!')
+        return render(request, 'encyclopedia/edit.html', {'body': entry, 'title': title})
+    raise Http404("Entry does not exist")
